27ebf5fbfee8f3e35e56680bd5bb1b00513fa505
cassio202021041 <cassio00710@hotmail.com>
2024-12-13 00:06:42 -0300

    teste
    commit teste

--- src/main/resources/db/migration/V01__criar_e_registrar_categoria.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categoria.sql
@@ -8,3 +8,9 @@ INSERT INTO categoria (nome) values ('Alimentação');
 INSERT INTO categoria (nome) values ('Supermercado');
 INSERT INTO categoria (nome) values ('Farmácia');
 INSERT INTO categoria (nome) values ('Outros');
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
